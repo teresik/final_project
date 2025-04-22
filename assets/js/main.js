d895fa27dba786c80b42c0a7d7dc87995830017c
teresik <74211814+teresik@users.noreply.github.com>
2025-04-22 20:49:28 +0200

    Add interactive hamburger menu animation
    Replaced existing navbar toggle logic with a new interactive animation for better visual feedback. Introduced redesigned CSS and JavaScript to support the animated hamburger menu, improving UI/UX. Updated HTML to integrate the new hamburger menu implementation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+$(document).ready(function(){
+  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+    $(this).toggleClass('open');
   });
-
 });
