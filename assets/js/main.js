2af2f23352e4996852ef91a601f77d77ffbaec27
Teresik <sheff20.152@gmail.com>
2025-04-26 18:20:35 +0200

    Refactor mobile navbar button implementation
    Replaced `#nav-icon2` with `.header__navbar-btn` for improved clarity and maintainability. Updated associated styles, HTML structure, and JavaScript interaction with a cleaner and more modular approach. Removed unused click handlers for other icon IDs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-    $(this).toggleClass('open');
-  });
-});
+$(".header__navbar-btn").click(function () {
+	$(this).toggleClass("active")
+})
